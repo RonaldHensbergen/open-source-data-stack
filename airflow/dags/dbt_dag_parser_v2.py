cd2795ab2bc196d93f630690984c8dc70dc5bdf2
Ronald Hensbergen <ronaldhensbergen@gmail.com>
2026-04-25 14:15:27 +0200

    Deleted files coming back and fixing dag parser error

--- airflow/dags/dbt_dag_parser_v2.py
+++ airflow/dags/dbt_dag_parser_v2.py
@@ -1,9 +1,9 @@
 import json
 import logging
 import os
-import subprocess
 from airflow.utils.task_group import TaskGroup
-from airflow.sdk import dag, task
+from airflow.sdk import task
+
 
 class DbtDagParser:
     """
@@ -29,7 +29,6 @@ class DbtDagParser:
         dbt_run_group_name="dbt_run",
         dbt_test_group_name="dbt_test",
     ):
-
         self.dag = dag
         self.dbt_global_cli_flags = dbt_global_cli_flags
         self.dbt_project_dir = dbt_project_dir
@@ -53,7 +52,6 @@ class DbtDagParser:
             file_content = json.load(f)
         return file_content
 
-
     def make_dbt_task(self, node_name, manifest_json, dbt_verb):
         """
         Takes the manifest JSON content and returns a BashOperator task
@@ -63,49 +61,45 @@ class DbtDagParser:
             manifest_json: manifest JSON
             dbt_verb: 'run' or 'test'
         Returns: A BashOperator task that runs the respective dbt command
-            """
-
-        @task.bash
-        def dbt_task_run_command(self, node_name, dbt_verb)-> str:
-            model_name = node_name.split(".")[-1]
-            return (
-                    f"dbt {self.dbt_global_cli_flags} {dbt_verb} "
-                    f"--target {self.dbt_target} --models {model_name} "
-                    f"--profiles-dir {self.dbt_profiles_dir} --project-dir {self.dbt_project_dir} "
-                    f"""--vars '{{"date": " {{{{ ds }}}} " }}'"""
-            )
-
-        @task.bash
-        def dbt_task_test_command(self, node_name, dbt_verb)-> str:
-            test_name = node_name.split(".")[-1]
-            return (
-                    f"dbt {self.dbt_global_cli_flags} {dbt_verb} "
-                    f"--target {self.dbt_target} --select {test_name} "
-                    f"--profiles-dir {self.dbt_profiles_dir} --project-dir {self.dbt_project_dir} "
-                    f"""--vars '{{"date": " {{{{ ds }}}} " }}'"""
-            )
+        """
+        # Store instance variables in local scope for closure
+        dbt_global_cli_flags = self.dbt_global_cli_flags
+        dbt_target = self.dbt_target
+        dbt_profiles_dir = self.dbt_profiles_dir
+        dbt_project_dir = self.dbt_project_dir
 
         if dbt_verb == "run":
             model_name = node_name.split(".")[-1]
             task_group = self.dbt_run_group
 
-            dbt_task = dbt_task_run_command(node_name, dbt_verb) \
-                .set(task_id=node_name, task_group=task_group) \
-                .set_downstream([]) \
-                .set_upstream([]) \
-                .set(dag=self.dag)
+            @task.bash(task_id=node_name, task_group=task_group)
+            def dbt_task_run_command() -> str:
+                return (
+                    f"dbt {dbt_global_cli_flags} run "
+                    f"--target {dbt_target} --models {model_name} "
+                    f"--profiles-dir {dbt_profiles_dir} --project-dir {dbt_project_dir} "
+                    f"""--vars '{{"date": " {{{{ ds }}}} " }}'"""
+                )
+
+            dbt_task = dbt_task_run_command()
 
         elif dbt_verb == "test":
             test_name = manifest_json["nodes"][node_name]["name"]
             task_group = self.dbt_test_group
 
-            dbt_task = dbt_task_test_command(node_name, dbt_verb) \
-                .set(task_id=node_name, task_group=task_group) \
-                .set_downstream([]) \
-                .set_upstream([]) \
-                .set(dag=self.dag)
+            @task.bash(task_id=node_name, task_group=task_group)
+            def dbt_task_test_command() -> str:
+                return (
+                    f"dbt {dbt_global_cli_flags} test "
+                    f"--target {dbt_target} --select {test_name} "
+                    f"--profiles-dir {dbt_profiles_dir} --project-dir {dbt_project_dir} "
+                    f"""--vars '{{"date": " {{{{ ds }}}} " }}'"""
+                )
+
+            dbt_task = dbt_task_test_command()
+
         else:
-            raise RuntimeError('Please select a dbt command between "run" or "test')
+            raise RuntimeError('Please select a dbt command between "run" or "test"')
 
         # Keeping the log output, it's convenient to see when testing the python code outside of Airflow
         logging.info("Created task: %s", node_name)
